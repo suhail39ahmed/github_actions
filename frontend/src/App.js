3df46dd22e4a030908c0d5e09720e55f918a46e9
Suhail Ahmed I <suhail39ahmed@gmail.com>
2024-12-27 19:11:47 +0800

    update the files

---
+++ frontend/src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function App() {
+  return (
+    <div style={{ textAlign: "center" }}>
+      <h1>Welcome to the React Frontend!</h1>
+    </div>
+  );
+}
+
+export default App;
